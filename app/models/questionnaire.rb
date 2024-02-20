41f46236aa2c7d3bd46fddbef68d36c4ce325a61
Ilya Obretetskiy <obreil54@gmail.com>
2024-02-20 13:34:55 +0000

    Added the first validations for my models, more may be required later

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,6 +1,8 @@
 class Questionnaire < ApplicationRecord
-  has_many :equipment_questionnaires
+  has_many :equipment_questionnaires, dependent: :destroy
   has_many :it_equipments, through: :equipment_questionnaires
   has_and_belongs_to_many :questions
   has_many :answers
+
+  validates :title, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   before_create :generate_code
   has_many :it_equipments
-  has_many :answers
+  has_many :answers, dependent: :destroy
   before_destroy :update_it_equipments_status
+
+  validates :first_name, :last_name, :email, :code, :status, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z'’\-\s]+\z/, message: "only allows letters, hyphens, apostrophes, and spaces" }
+
+
 
   def generate_code
     self.code = SecureRandom.random_number(10000).to_s.rjust(4, '0')
