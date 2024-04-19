d3d4e56b324e5b6914960059de57007663225fad
debian <debian@it-audit.magnat.lan>
2024-04-19 12:25:06 +0300

    fixed docker-compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :cloudianry
+#  config.active_storage.service = :cloudianry
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -94,4 +94,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         '192.168.4.51',
+    port:            25,
+  enable_starttls: false,
+  open_timeout:    5,
+  read_timeout:    5 }
+
+
+
+
 end
