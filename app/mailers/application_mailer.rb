83caa0365d8e7b61ff232c8b59a57f5bc4b93a31
Ilya Obretetskiy <obreil54@gmail.com>
2024-05-17 15:37:39 +0100

    Created a settings table and added the email_system_name logic for login code emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "obreil54@gmail.com"
+  default from: -> { system_email_address }
   layout "mailer"
+
+  private
+
+  def system_email_address
+    Setting.email_system_name
+  end
 end
