b88877e9b10298c8bc791a22a36f6b0eba89c18c
Ilya Obretetskiy <obreil54@gmail.com>
2024-09-08 06:08:52 +0100

    Disabled caching to try and fix the problem

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", to: "application.js", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: false
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: false
+pin_all_from "app/javascript/controllers", under: "controllers", preload: false
+pin "bootstrap", to: "bootstrap.min.js", preload: false
+pin "@popperjs/core", to: "popper.js", preload: false
